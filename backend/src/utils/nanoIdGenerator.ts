56ba361fff2853719f6f501bbf6fdae89e618938
pratap562 <kumpratap562@gmail.com>
2026-01-16 08:35:02 +0530

    ld

--- backend/src/utils/nanoIdGenerator.ts
+++
@@ -1,9 +0,0 @@
-import { customAlphabet } from 'nanoid';
-
-const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-const generateId = customAlphabet(alphabet, 12);
-
-export const generateNanoId = async (): Promise<string> => {
-  return generateId();
-};
-
